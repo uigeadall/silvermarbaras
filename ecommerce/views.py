a9edcb5ae30619448427d8e5e8a279a172b56c46
uigeadall <162350255+uigeadall@users.noreply.github.com>
2025-11-21 13:50:44 +0200

    Add ensure_csrf_cookie decorator to home view to ensure CSRF cookie is set

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -34,7 +34,7 @@ from django.core.exceptions import ValidationError
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.views.decorators.http import require_http_methods, require_POST
 
 from .forms import RatingForm
