ba5690603e0d82630bc49221b142619fc5a14f83
uigeadall <162350255+uigeadall@users.noreply.github.com>
2025-12-09 16:41:33 +0200

    Import template tags in AppConfig.ready() to ensure they are registered

--- ecommerce/apps.py
+++ ecommerce/apps.py
@@ -6,5 +6,11 @@ class EcommerceConfig(AppConfig):
     name = "ecommerce"
 
     def ready(self):
-
         from . import signals
+        
+        # Import template tags to ensure they are registered
+        try:
+            import ecommerce.template_tags.blog_filters  # noqa
+            import ecommerce.template_tags.cart_extras  # noqa
+        except ImportError:
+            pass
