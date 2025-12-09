de2b376da330865c451b40189bf2cdabb048a9c1
uigeadall <162350255+uigeadall@users.noreply.github.com>
2025-12-09 12:16:49 +0200

    Fix login: use login_view function instead of CustomLoginView

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -35,8 +35,7 @@ path("account/", views.profile_dashboard, name="profile_dashboard"),
 path('checkout/guest/', views.guest_checkout_view, name='guest_checkout'),
 
 
-    path("login/", CustomLoginView.as_view(), name="account_login"),
-path("login/", CustomLoginView.as_view(), name="login"),
+    path("login/", views.login_view, name="login"),
     path("health/", views.health_check, name="health_check"),
     path("test-emails/", views.test_emails_view, name="test_emails"),
 ]
